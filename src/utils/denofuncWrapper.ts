f97d66b8009ae3309313dafcde7375f998cf6b4c
horihiro <hori.kawa.shima.tool+github@gmail.com>
2021-03-01 21:05:42 +0900

    Add setting for path of `denofunc`

--- src/utils/denofuncWrapper.ts
+++ src/utils/denofuncWrapper.ts
@@ -1,11 +1,13 @@
 'use strict';
 
-import { OutputChannel } from 'vscode';
+import { OutputChannel, workspace } from 'vscode';
 import { ExecOptions } from 'child_process';
 import { spawnAsync } from './spawn';
 
+
 export async function execInitProject(options: ExecOptions, channel?: OutputChannel) {
-  return await spawnAsync('denofunc init', options, channel);
+  const pathToDenoFunc = workspace.getConfiguration('denofunc')?.path || 'denofunc';
+  return await spawnAsync(`${pathToDenoFunc} init`, options, channel);
 }
 
 export async function execDeploy(appName: string, options: ExecOptions, channel?: OutputChannel) {
@@ -13,5 +15,6 @@ export async function execDeploy(appName: string, options: ExecOptions, channel?
 }
 
 export async function execDeploySlot(appName: string, slotName: string | null, options: ExecOptions, channel?: OutputChannel) {
-  return await spawnAsync(`denofunc publish ${appName}${slotName ? ` --slot ${slotName}` : ''}`, options, channel);
+  const pathToDenoFunc = workspace.getConfiguration('denofunc')?.path || 'denofunc';
+  return await spawnAsync(`${pathToDenoFunc} publish ${appName}${slotName ? ` --slot ${slotName}` : ''}`, options, channel);
 }
