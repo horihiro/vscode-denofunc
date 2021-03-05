b5c9744811c835a61ce03a6a033962c5dc923815
horihiro <hori.kawa.shima.tool+github@gmail.com>
2021-03-05 20:28:58 +0900

    Fix folder path operation

--- src/utils/getTargetFolder.ts
+++ src/utils/getTargetFolder.ts
@@ -4,7 +4,10 @@ import { window, workspace, QuickPickItem, QuickPickOptions } from 'vscode';
 
 export async function getTargetFolder(options: QuickPickOptions): Promise<QuickPickItem | undefined> {
   if (!workspace.workspaceFolders) return undefined;
-  if (workspace.workspaceFolders.length === 1) return { label: workspace.workspaceFolders[0].name, description: workspace.workspaceFolders[0].uri.fsPath };
+  if (workspace.workspaceFolders.length === 1) {
+    const wf = workspace.workspaceFolders[0];
+    return { label: wf.name, description: wf.uri.fsPath.replace(new RegExp(wf.name), '') };
+  }
   return await window.showQuickPick(
     workspace.workspaceFolders.map(wf => ({
       label: wf.name,
